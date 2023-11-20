85bb2fe4baa2d056147756a8552d1d5e663fb6ad
ys yasin arafat <ysyasin7.6@gmail.com>
2023-11-20 15:47:04 +0600

    add tag input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tagValue, setTagValue] = useState("");
+  const [tag, setTag] = useState([]);
+
+  const addTag = (e) => {
+    // console.log(e.);
+    if (e.keyCode === 13 && tagValue) {
+      setTag([...tag, tagValue]);
+      setTagValue("");
+      document.getElementById("tagInput").value = "";
+    }
+  };
+
+  const handleDeletTagItem = (id) => {
+    const remaining = tag.filter((t) => tag.indexOf(t) !== id);
+    setTag(remaining);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="tex-center">Add Tag</h1>
+
+      <div
+        style={{ border: "1px solid gray " }}
+        className="w-[300px] p-2 h-auto rounded-md max-w-xs my-5 flex flex-wrap"
+      >
+        {tag.map((t, index) => (
+          <button
+            onClick={() => handleDeletTagItem(index)}
+            key={index}
+            className="px-3 py-2 m-1 rounded-lg"
+          >
+            {t} <span className="me-1">x</span>
+          </button>
+        ))}
+        <input
+          onChange={(e) => setTagValue(e.target.value)}
+          onKeyUp={addTag}
+          type="text"
+          id="tagInput"
+          defaultValue={tagValue}
+          className="focus:border-none focus:outline-none max-w-full"
+          placeholder="add tag click here to"
+        />
+      </div>
+    </>
   );
 }
 
